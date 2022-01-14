9431b282b32c8ec40d0bdcd74de7188b881705e1
Hartmut Bauer <hb42@gmx.de>
2022-01-15 00:15:01 +0100

    DNS lookup for apname

--- src/preload.ts
+++ src/preload.ts
@@ -3,12 +3,17 @@
  *  -> https://www.electronjs.org/docs/latest/api/context-bridge
  */
 import { execFile } from "child_process";
+import { promises } from "dns";
 import { contextBridge } from "electron";
 import { Config } from "./config/config";
 
 const APIKEY = "electron";
 const config = new Config();
 
+const ipString =
+  /^\s*(\d|[01]?\d\d|2[0-4]\d|25[0-5])\.(\d|[01]?\d\d|2[0-4]\d|25[0-5])\.(\d|[01]?\d\d|2[0-4]\d|25[0-5])\.(\d|[01]?\d\d|2[0-4]\d|25[0-5])\s*$/;
+
+// API fuer den Aufruf aus der Anwendung
 // class o.ae. funktioniert hier nicht, nur simples object
 const api = {
   test: (msg: string) =>
@@ -18,10 +23,33 @@ const api = {
 
   version: () => process.versions.electron,
 
-  exec: (job: string, ap) => {
+  exec: async (job: string, ap): Promise<{ rc: number; info: string }> => {
     console.debug("sbsdb-local exec: " + job);
     console.dir(ap);
 
+    let hostname = "";
+    // DNS-Lookup
+    let ipAddr = (await promises
+      .lookup(ap.apname, { family: 4 })
+      .then((result) => result.address)
+      .catch(() => null)) as string;
+    if (ipAddr) {
+      // hostname gefunden
+      hostname = ap.apname as string;
+    } else {
+      // keine Namensaufloesung
+      if (ipString.test(ap.ipStr)) {
+        ipAddr = ap.ipStr as string;
+      } else {
+        // und keine gueltige IP-Adresse
+        return {
+          rc: 1,
+          info:
+            "Hostname kann nicht aufgelöst werden und es ist keine " +
+            "gültige IP-Adresse in der Datenbank vorhanden",
+        };
+      }
+    }
     const json = JSON.stringify(ap, (key, value: unknown) => {
       // moegliche Rekursionen aus dem Objekt entfernen
       if (key === "ap" || key === "children") {
@@ -31,15 +59,26 @@ const api = {
     }).replace(/"/g, "'");
 
     const shell = config.shell;
-    const parameters = [config.script, "-job", job, "-ap", '"' + json + '"'];
+    const parameters = [
+      config.script,
+      "-job",
+      job,
+      "-hostname",
+      hostname,
+      "-ip",
+      ipAddr,
+      "-ap",
+      '"' + json + '"',
+    ];
 
-    execFile(shell, parameters, (err, data) => {
+    execFile(shell, parameters, (err, stdout, errout) => {
       if (err) {
         console.error(err);
-        return;
       }
-      console.log(data.toString());
+      console.debug(stdout);
+      console.debug(errout);
     });
+    return { rc: 0, info: "OK" };
   },
 };
 
